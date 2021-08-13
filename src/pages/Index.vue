9460ce8a8136b5d91a23d90000fa33bc61cc5549
BGeoffroy <bengeoffroy2@gmail.com>
2021-08-13 16:02:45 -0500

    main

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,116 @@
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
+
+
+<script src = "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
+    <label for="un">Username</label>
+    <input id="un" type="text" placeholder="Enter Username" v-model="username" />
+    
+    <label for="pw">Password</label>
+    <input id="pw" type="password" placeholder="Enter Password" v-model="password" />
+    <button type="submit" v-on:click="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+    <label>{{username}}</label>
+    <button type="button" class="cancelbtn">Cancel</button>
 
   </Layout>
 </template>
 
 <script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    const app = new Vue({
+        el: '#app',
+        data: {
+            username: '',
+            password: '',
+            shoppingCart: [
+                { label: 'Apples', cost: 6 },
+                { label: 'Bananas', cost: 2 },
+                { label: 'Coconuts', cost: 8 }
+            ]
+        },
+        
+        computed: {
+        },
+        methods: {
+            submit() {
+                alert("You submitted Username:" + this.username + " and Password:" + this.password)
+                console.log("sending")
+                socket.emit("authorization", this.username)
+                console.log("sent!")
+            }
+        }
+
+    })
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+
+
+
