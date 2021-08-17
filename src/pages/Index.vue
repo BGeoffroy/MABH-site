8a95b9c29def2ab02d0dcfcf6bbf5636078e6461
BGeoffroy <bengeoffroy2@gmail.com>
2021-08-17 11:13:01 -0500

    message

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
+  <Layout>
+    <h1>Welcome to MABH!</h1>
+    <p>Jason and Mitchell, this is the "index.vue" file that defines the home page for this site. The goal is to use these pages (and a login page) to programmatically generate user pages that can define a user network. </p>
+  </Layout>
 </template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home'
+  }
+}
+</script>
